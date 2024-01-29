dbb201827cd3b25a63b6fb1ae6a09a1a3369bdae
Vincent Klimjack <vklimjack@gmail.com>
2024-01-29 14:28:37 +0000

    implement ValidateInputAndDivideFood func

--- the-farm/the_farm.go
+++ the-farm/the_farm.go
@@ -1,5 +1,7 @@
 package thefarm
 
+import "errors"
+
 func DivideFood(fodderCalculator FodderCalculator, numberOfCows int) (float64, error) {
 	fodderAmount, err := fodderCalculator.FodderAmount(numberOfCows)
 
@@ -17,18 +19,13 @@ func DivideFood(fodderCalculator FodderCalculator, numberOfCows int) (float64, e
 }
 
 func ValidateInputAndDivideFood(fodderCalculator FodderCalculator, numberOfCows int) (float64, error) {
-	panic("")
+	if numberOfCows <= 0 {
+		return 0, errors.New("invalid number of cows")
+	}
+
+	return DivideFood(fodderCalculator, numberOfCows)
 }
 
 func ValidateNumberOfCows(numberOfCows int) error {
 	panic("")
 }
-
-// Your first steps could be to read through the tasks, and create
-// these functions with their correct parameter lists and return types.
-// The function body only needs to contain `panic("")`.
-//
-// This will make the tests compile, but they will fail.
-// You can then implement the function logic one by one and see
-// an increasing number of tests passing as you implement more
-// functionality.
